163e7bad050fad22272ce8131c18279edac95188
Amriabochkina <Anna.rm07@mail.ru>
2025-02-12 20:33:50 +0300

    without styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Elements } from "./components/Elements";
+var container = []
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      elements: [
+        "горький", "сладкий", "кислый"
+      ]
+    }
+    for (var i = 0; i < this.state.elements.length; i++) {
+      container[i] = 0
+    }
+  }
+  render(){
+    return (
+      <div className="wrapper">
+          <Header />
+          <Elements elements={this.state.elements} choose={this.choose}/>
+          <Footer />
+        </div>
+    );
+  }
+  choose(value, name){
+    container[name] = value
+  }
 }
-
 export default App;
